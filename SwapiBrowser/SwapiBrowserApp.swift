ff7781106785e7e4564004d567d944ac8cc42cdc
Gunnar Lieder <gunnar.lieder@googlemail.com>
2023-07-28 10:13:13 +0200

    Initial Commit

---
+++ SwapiBrowser/SwapiBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwapiBrowserApp.swift
+//  SwapiBrowser
+//
+//  Created by Admin on 28.07.23.
+//
+
+import SwiftUI
+
+@main
+struct SwapiBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
